7d011a34a810e11ae3609b25802a0448318de8a8
JUAN GABRIEL ESTRADA BECERRA <jugarestrada@gmail.com>
2025-04-26 13:20:22 -0600

    Actualización de configuración y eliminación de archivo de imagen

---
+++ android/app/src/main/java/com/unicorniolector/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.unicorniolector.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,36 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.unicornio.lector',
-  appName: 'Unicornio Lector',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https',
-    cleartext: true,
-    url: 'https://b7c71a89-bc3f-4f91-a063-782dca0f8ed9.lovableproject.com?forceHideBadge=true'
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      backgroundColor: '#a78bfa',
-      showSpinner: true,
-      androidSpinnerStyle: 'large',
-      splashFullScreen: true,
-      splashImmersive: true
-    }
-  },
-  android: {
-    buildOptions: {
-      minSdkVersion: 22,
-      targetSdkVersion: 33,
-      versionCode: 1,
-      versionName: "1.0.0",
-      keystoreAlias: "unicorniolector",
-      keystorePassword: "unicorniolector",
-      packageName: "com.unicornio.lector"
-    }
-  }
+  appId: 'com.unicorniolector.app',
+  appName: 'Unicorniolector',
+  webDir: 'dist'
 };
 
 export default config;
